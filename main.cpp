e69f4a3f9436164bcf7f125c789b53646431eada
Sukiu <sukiu59170@hotmail.com>
2019-11-06 19:50:57 +0100

    v1

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main(){
+
+    unsigned int a = 4294967299;
+
+
+    unsigned int b = 4294967296;
+    // Somme x_k * 2^32^k
+
+    //[1,3] * [1,0]
+    //[1,0]
+
+
+    // 11
+    //*10
+    // 9
+    //
+    return 0;
+}
+
